68a564168883ae320c7017fa3fb70859c4ea1b0e
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-03-24 11:47:12 +0100

    Create TodoList component, not used yet

---
+++ SUT25-React/src/components/TodoList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function TodoList() {
+    return (<h1>"Todo List Component"</h1>);
+
+}
+
+/* Main component 
+   
+   Add some hardcoded todos in state
+
+   Connect TodoInput and TodoItem here
+
+   Implement toggleTodo and deleteTodo fx and send out props
+
+   Add Styling and DONE marking
+
+   Add useEffect logic for reading and writting todos in LocalStorage
+
+   Test and refresh the site and see if the todos are saved
+
+   1-2 extrafunktions 
+   */
+  export default TodoList
