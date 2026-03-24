893779d7a4adb2d3cf6520d7f62194270676d13b
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-03-24 17:48:14 +0100

    Updated TodoInput so the UI allows the user to enter a new todo

--- SUT25-React/src/components/TodoInput.jsx
+++ SUT25-React/src/components/TodoInput.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import { useState } from 'react';
 
-function TodoInput() {
-    const simpleComponent = "Todo Input";
-    return <h1>{simpleComponent}</h1>;
+function TodoInput({ onAddTodo }) {
+  const [value, setValue] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (onAddTodo) {
+      onAddTodo(value);
+    }
+
+    // Clean up the input field afterwards
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Skriv en todo..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)} // update with any keytouch
+      />
+      <button type="submit">Lägg till</button>
+    </form>
+  );
 }
 
-/* 
-
-*/
 export default TodoInput;
