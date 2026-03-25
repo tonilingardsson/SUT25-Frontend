47b7e4be30067f481c5d9765a8a05d3c52180689
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-03-25 10:49:59 +0100

    TodoInput handles all the input data  from the user for new todos

--- SUT25-React/src/components/TodoInput.jsx
+++ SUT25-React/src/components/TodoInput.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 
 function TodoInput({ onAddTodo }) {
-  const [value, setValue] = useState('');
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
+  const [deadline, setDeadline] = useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    if (onAddTodo) {
-      onAddTodo(value);
-    }
+    if (!onAddTodo) return;
+
+    onAddTodo(text,category, deadline);
 
     // Clean up the input field afterwards
-    setValue('');
+    setText('');
+    setCategory('');
+    setDeadline('');
   }
 
   return (
@@ -19,9 +23,25 @@ function TodoInput({ onAddTodo }) {
       <input
         type="text"
         placeholder="Skriv en todo..."
-        value={value}
-        onChange={(e) => setValue(e.target.value)} // update with any keytouch
+        value={text}
+        onChange={(e) => setText(e.target.value)} // update with any keytouch
       />
+      <select
+      value={category}
+      onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value={""}>Välj kategori</option>
+        <option value={"arbete"}>Arbete</option>
+        <option value={"privat"}>Privat</option>
+        <option value={"studier"}>Studier</option>
+      </select>
+
+      {/* Date field for deadline */}
+      <input type="date"
+      value={deadline}
+      onChange={(e) => setDeadline(e.target.value)} 
+      />
+      
       <button type="submit">Lägg till</button>
     </form>
   );
